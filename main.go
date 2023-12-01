f06ca783bcf10afcc9bd9e6d18445c3b5c583a50
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-01 18:51:20 +0800

    added clear screen

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	// EncodeImage()
 	// DecodeImage()
 
+	mainMenu()
 }
 
 func mainMenu() {
-	fmt.Println("main menu")
+	clearScreen()
+	fmt.Println("pick an option: ")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	options, _ := reader.ReadString('\n')
+
+	switch strings.TrimSpace(options) {
+	case "encode":
+		fmt.Println("you picked encode")
+	case "decode":
+		fmt.Println("you picked decode")
+	default:
+		mainMenu()
+	}
 }
 
 // go run main.go encode.go decode.go
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
+}
