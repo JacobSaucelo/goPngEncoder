5c6f49d75609fa0ad6cbc254b9260c553c47155c
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-01 13:16:41 +0800

    done woring on decode

--- decode.go
+++ decode.go
@@ -6,6 +6,7 @@ import (
 	"image/color"
 	"image/png"
 	"os"
+	"strconv"
 )
 
 type Decode struct {
@@ -14,7 +15,7 @@ type Decode struct {
 
 func DecodeImage() {
 	RawImage := NewDecode()
-	RawImage.ImageToBits()
+	RawImage.ImageToText()
 }
 
 func NewDecode() Decode {
@@ -36,7 +37,7 @@ func NewDecode() Decode {
 	}
 }
 
-func (rawImage *Decode) ImageToBits() {
+func (rawImage *Decode) ImageToText() {
 	bounds := rawImage.data.Bounds()
 	bitString := ""
 
@@ -46,18 +47,44 @@ func (rawImage *Decode) ImageToBits() {
 			// fmt.Printf("(1,%d)\n", x)
 			pixelColor := rawImage.data.At(x, y)
 			rgba := color.RGBAModel.Convert(pixelColor).(color.RGBA)
-			fmt.Printf("Pixel at (%d, %d): R:%d G:%d B:%d A:%d\n", x, y, rgba.R, rgba.G, rgba.B, rgba.A)
+			// fmt.Printf("Pixel at (%d, %d): R:%d G:%d B:%d A:%d\n", x, y, rgba.R, rgba.G, rgba.B, rgba.A)
 
 			if (rgba == color.RGBA{255, 255, 255, 255}) {
-				fmt.Print(" white")
 				bitString += "1"
 			} else {
-				fmt.Print(" black")
 				bitString += "0"
-
 			}
 		}
 	}
 
-	fmt.Println("raw Image: ", bitString)
+	// fmt.Println("raw Image: ", bitString)
+	RawBytes := binaryToBytes(bitString)
+	CreateText(RawBytes)
+}
+
+func binaryToBytes(bitString string) []byte {
+	// check if divisible by 8 - adds 1 index on length
+	for len(bitString)%8 != 0 {
+		bitString = "0" + bitString
+	}
+
+	bytes := make([]byte, len(bitString)/8)
+
+	for i := 0; i < len(bitString); i += 8 {
+		chunk := bitString[i : i+8]
+		value, _ := strconv.ParseUint(chunk, 2, 8)
+
+		bytes[i/8] = byte(value)
+	}
+
+	return bytes
+}
+
+func CreateText(bytes []byte) {
+	FileError := os.WriteFile("./output/decoded.txt", bytes, 0644)
+	if FileError != nil {
+		fmt.Println("Error:", FileError)
+	} else {
+		fmt.Println("Text file 'output.txt' created successfully.")
+	}
 }
